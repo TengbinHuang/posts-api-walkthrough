32bf46d58252c66d1c89bac9330fa00c26dd2239
TengbinHuang <52409477+TengbinHuang@users.noreply.github.com>
2021-03-24 18:25:56 +1100

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'jr-posts-app',
+        script: './src/index.js',
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
+};
